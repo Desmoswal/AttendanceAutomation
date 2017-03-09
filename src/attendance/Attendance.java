f11bdc4df2cf6be5a0ebb582de411f766e8758f9
kopicon <boldi123@gmail.com>
2017-03-09 15:33:36 +0100

    Some fixes
    -Admin logout button and function
    -CSS parts
    -Fxmls

--- src/attendance/Attendance.java
+++ src/attendance/Attendance.java
@@ -15,6 +15,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -35,6 +36,7 @@ public class Attendance extends Application
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("attendance/EASV.png"));
         stage.show();
         
         //getStudents();
